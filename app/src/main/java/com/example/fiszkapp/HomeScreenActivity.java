282eaf072ff5e25c28518db08d0ceb5441c551f7
ligarius20fps <ligarius20fps@users.noreply.github.com>
2021-11-04 19:26:14 +0100

    nie chce mi sie

--- app/src/main/java/com/example/fiszkapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/fiszkapp/HomeScreenActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//to powinno być MainActivity
 public class HomeScreenActivity extends AppCompatActivity {
     TextView labelWelcome;
     Bundle args;
