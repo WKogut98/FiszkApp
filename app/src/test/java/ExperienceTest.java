d968ca4434f44cc8fcb198f1e267e1055f1c491f
ligarius20fps <ligarius20fps@users.noreply.github.com>
2021-11-25 18:19:08 +0100

    Poprawka testów

--- app/src/test/java/ExperienceTest.java
+++ app/src/test/java/ExperienceTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import com.example.fiszkapp.Experience;
+
 import org.junit.Test;
 
 public class ExperienceTest {
@@ -7,19 +9,26 @@ public class ExperienceTest {
     @Test
     public void calculateNeededExp() {
         int level = 4;
-        assertEquals(1750,Experience.calculateNeededExp(level));
+        assertEquals(1750, Experience.calculateNeededExp(level));
     }
 
     @Test
     public void calculateExpFromCurrentLevel() {
-        Experience.calculateExpFromCurrentLevel()
+        int level = 4;
+        int exp = 4000;
+        assertEquals(250,Experience.calculateExpFromCurrentLevel(exp,level));
     }
 
     @Test
     public void totalExpToGetLevel() {
+        int level = 4;
+        assertEquals(3750,Experience.totalExpToGetLevel(level));
     }
 
     @Test
     public void expToNextLevel() {
+        int exp = 5000;
+        int level = 4;
+        assertEquals(500,Experience.expToNextLevel(exp,level));
     }
 }
