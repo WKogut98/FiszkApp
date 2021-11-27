2eb905279041b1bf65c3d9f4530e23689cca635f
ligarius20fps <ligarius20fps@users.noreply.github.com>
2021-11-27 20:13:11 +0100

    Update SelectCollectionActivity.java

--- app/src/main/java/com/example/fiszkapp/SelectCollectionActivity.java
+++ app/src/main/java/com/example/fiszkapp/SelectCollectionActivity.java
@@ -2,10 +2,14 @@ package com.example.fiszkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,21 +20,31 @@ public class SelectCollectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_collection);
 
-        //do adaptera trzeba tablicę Stringów
+        //do adaptera trzeba listę Stringów
         DBHelper helpMe = new DBHelper(this);
         Cursor cursor = helpMe.getAllData(DBHelper.table_names[1]);
         ArrayList<String> collectionNames = new ArrayList<>();
+        ArrayList<Integer> idList = new ArrayList<>();
         while(cursor.moveToNext())
         {
             collectionNames.add(cursor.getString(1));
+            idList.add(cursor.getInt(0));
         }
         if(collectionNames.isEmpty())
         {
-            //przejdź do aktywności dodania kolekcji
-            //finish();
+            Toast.makeText(this, "Przed dodaniem fiszki dodaj kolekcję do której będą dodawane fiszki", Toast.LENGTH_LONG).show();
+            Intent toAddCollecion = new Intent(SelectCollectionActivity.this, CollectionsActivity.class);
+            startActivity(toAddCollecion);
+            finish();
         }
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.activity_select_collection, collectionNames);
         ListView listView = (ListView)findViewById(R.id.collectionList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent toAddFlashcard = new Intent(SelectCollectionActivity.this, NewFlashcardActivity.class);
+            }
+        });
     }
 }
