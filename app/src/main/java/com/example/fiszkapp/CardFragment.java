2b425a98db614a18bbbca087270bb9a3d8c4eceb
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2022-01-20 19:14:59 +0100

    fragment z przeglądaniem fiszek

--- app/src/main/java/com/example/fiszkapp/CardFragment.java
+++ app/src/main/java/com/example/fiszkapp/CardFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,8 +23,10 @@ public class CardFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String frontText;
+    private String backText;
+    TextView textWord;
+    Button buttonFlip;
 
     public CardFragment() {
         // Required empty public constructor
@@ -50,15 +54,31 @@ public class CardFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            frontText = getArguments().getString(ARG_PARAM1);
+            backText = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false);
+        View view=inflater.inflate(R.layout.fragment_card, container, false);;
+        textWord=view.findViewById(R.id.TextWord);
+        textWord.setText(frontText);
+        buttonFlip=view.findViewById(R.id.buttonFlip);
+        buttonFlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(textWord.getText().equals(frontText))
+                {
+                    textWord.setText(backText);
+                }
+                else
+                {
+                    textWord.setText(frontText);
+                }
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/fiszkapp/LessonActivity.java
+++ app/src/main/java/com/example/fiszkapp/LessonActivity.java
@@ -1,14 +1,25 @@
 package com.example.fiszkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class LessonActivity extends AppCompatActivity {
 
+    String front;
+    String back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+        //utworzenie menedżera fragmentu, żeby wstawić do niego dany fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().
+                setReorderingAllowed(true).
+                add(R.id.fragmentContainerView, CardFragment.newInstance(front, back), null)
+                .commit();
     }
 }
